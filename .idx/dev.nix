be44c3def80bf86434e753b751549cf453bfcb03
night.owl.codeguru <night.owl.codeguru@gmail.com>
2025-05-07 03:33:51 +0000

    nix edit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -21,32 +21,18 @@
     ];
 
     # Enable previews
-    previews = {
+    previews = { 
       enable = false;
       previews = {
-        # web = {
-        # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        # and show it in IDX's web preview panel
-        #  command = ["cd module_2" "flutter" "run"];
-        #   manager = "web";
-        # env = {
-        # Environment variables to set for your server
-        #   PORT = "$PORT";
-        #  };
-        #  };
+        
       };
     };
 
     # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        flutter-install = "flutter pub get";
       };
-      # Runs when the workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
         run-module-2 = "cd module_2 && flutter run -d web-server";
         run-module-3 = "cd module_3 && flutter run -d web-server";
         run-module-5-6 = "cd module_5_&_6 && flutter run -d web-server";
@@ -55,7 +41,7 @@
         run-module-11-12 = "cd module_11_12 && flutter run -d web-server";
         run-module-13 = "cd module_13 && flutter run -d web-server";
         run-module-14 = "cd module_14 && flutter run -d web-server";
-              };
+      };
     };
   };
 }
